f60f4b339d1a6cd8d48a679cae0a316f654851fd
analogkeyboard <104320272+analogkeyboard@users.noreply.github.com>
2023-09-16 19:12:40 -0700

    hoja de estilos agregado

---
+++ procesar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Respuesta</title>
+</head>
+<body>
+    <section>
+        <p>
+            <?php
+print("el valor es: ".$_GET['nombre']);
+            ?>
+        </p>
+    </section>
+</body>
+</html>
